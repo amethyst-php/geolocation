5661bc764b9e3ac4ec9a7d3f290b14b051d5d0c9
railken <railken@protonmail.ch>
2019-05-06 00:25:01 +0200

    update

--- database/migrations/0000_00_00_000000_create_geolocation_points_table.php
+++ database/migrations/0000_00_00_000000_create_geolocation_points_table.php
@@ -18,8 +18,7 @@ class CreateGeolocationPointsTable extends Migration
             $table->float('longitude');
             $table->string('localizable_type')->nullable();
             $table->integer('localizable_id')->unsigned()->nullable();
-            $table->index(['localizable_type', 'localizable_id']);
-            $table->index(['localizable_type', 'localizable_id', 'deleted_at']);
+            $table->index(['localizable_type', 'localizable_id'], 'localizable_index');
             $table->timestamps();
             $table->softDeletes();
         });
