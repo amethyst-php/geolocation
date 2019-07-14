c56d6f5e1a246111107e653b16c848701d174912
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/GeolocationPointManager.php
+++ src/Managers/GeolocationPointManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\GeolocationPoint newEntity()
+ * @method \Amethyst\Schemas\GeolocationPointSchema getSchema()
+ * @method \Amethyst\Repositories\GeolocationPointRepository getRepository()
+ * @method \Amethyst\Serializers\GeolocationPointSerializer getSerializer()
+ * @method \Amethyst\Validators\GeolocationPointValidator getValidator()
+ * @method \Amethyst\Authorizers\GeolocationPointAuthorizer getAuthorizer()
+ */
 class GeolocationPointManager extends Manager
 {
     use ConfigurableManager;
