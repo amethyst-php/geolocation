121f20b2888f7d954844f523eefbb7b04ed37c2f
railken <railken@protonmail.ch>
2019-07-14 16:49:54 +0200

    cleanup

--- src/Providers/GeolocationServiceProvider.php
+++ src/Providers/GeolocationServiceProvider.php
@@ -6,19 +6,5 @@ use Amethyst\Common\CommonServiceProvider;
 
 class GeolocationServiceProvider extends CommonServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register()
-    {
-        parent::register();
-    }
-
-    /**
-     * @inherit
-     */
-    public function boot()
-    {
-        parent::boot();
-    }
+    // ...
 }
